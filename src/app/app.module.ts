3d54837ea4d48a9011ddb84bf7db25813df4d154
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-07-30 14:35:16 +0200

    use data service to get customers list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { CustomersModule } from './customers/customers.module';
+ import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  imports: [BrowserModule, CustomersModule, SharedModule , CoreModule],
+  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
