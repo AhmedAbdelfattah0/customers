a881e70fe3a7b4f1c109447ce2750794c8b79ad2
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-07-30 15:15:21 +0200

    add routing to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule],
+  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
