b44b5429d26fe2f6b66d17369be1f5e08bb416bc
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-08-05 11:36:00 +0200

    create link on customer name to get his orders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, CoreModule, CustomersModule,OrdersModule, SharedModule, AppRoutingModule, ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
